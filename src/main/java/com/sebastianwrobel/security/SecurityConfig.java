b215ccb60ed7920c2d26eec5fb661cd38a4bf6b3
zzyzzyxx <molken24@gmail.com>
2023-08-26 15:09:35 +0200

    Security added

---
+++ src/main/java/com/sebastianwrobel/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.sebastianwrobel.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("John").password(passwordEncoder().encode("admin")).roles("ADMIN");
+	}
+	
+}
+
