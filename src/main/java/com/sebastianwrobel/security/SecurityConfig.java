8cf9ce2a3bca06816d8f5be0ffc0200b25cff610
zzyzzyxx <molken24@gmail.com>
2023-08-26 15:19:18 +0200

    Roles added

--- src/main/java/com/sebastianwrobel/security/SecurityConfig.java
+++ src/main/java/com/sebastianwrobel/security/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.sebastianwrobel.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Bean
@@ -19,7 +22,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 			.inMemoryAuthentication()
-			.withUser("John").password(passwordEncoder().encode("admin")).roles("ADMIN");
+			.withUser("John").password(passwordEncoder().encode("admin")).roles("ADMIN", "EMPLOYEE")
+			.and()
+			.withUser("Eric").password(passwordEncoder().encode("employee")).roles("EMPLOYEE")
+			.and()
+			.withUser("Michael").password(passwordEncoder().encode("client")).roles("CLIENT");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/", "/login")
+				.permitAll()
+			.and()
+				.formLogin()
+				.defaultSuccessUrl("/")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutSuccessUrl("/")
+				.invalidateHttpSession(true)
+				.permitAll();
 	}
 	
 }
